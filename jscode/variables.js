166eeeedeccb95d1d0d4d248cbc706aad59ac955
SHASHWAT SHUKLA <105039107+Shashwatshukl2040@users.noreply.github.com>
2024-05-15 19:57:24 +0000

    done

---
+++ jscode/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail="hi@gamil.com"
+var accPass= "1232"
+accCity="Prayagraj"
+// accountId =2
+
+accountEmail="GAGANGD"
+accPass="12"
+accCity="132"
+console.log(accountId);
+console.table([accountEmail, accPass,accountId, accCity])
