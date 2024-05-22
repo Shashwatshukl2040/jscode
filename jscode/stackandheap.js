f438df16689167b13ecea90ea61622c145be2029
SHASHWAT SHUKLA <105039107+Shashwatshukl2040@users.noreply.github.com>
2024-05-22 07:25:42 +0000

    memory

---
+++ jscode/stackandheap.js
@@ -0,0 +1,17 @@
+// stack(primitive) and heap(non-primitive)
+ 
+let myyt = "hhasdhfasdfsad"
+let anname= myyt
+anname="atulshukla"
+console.log(anname)
+console.log(myyt)
+let user1= {
+    email: "user@gmail.com",
+    upi:"uesr1@ptsbi"
+
+}
+let user2=user1
+user2.email= "atul@google.com"
+
+console.log(user1.email)
+console.log(user2.email)
