051533f9da7147c3d88efa1b5c13da42df51b6b6
SHASHWAT SHUKLA <105039107+Shashwatshukl2040@users.noreply.github.com>
2024-05-22 06:41:35 +0000

    all datatypes

---
+++ jscode/datatypes.js
@@ -0,0 +1,48 @@
+// Primitive Datatypes
+    /* 7 types
+        String
+        number
+        boolean
+        null
+        undefined
+        symbol
+        BigInt
+*/
+const score = 100
+const svalue =100.3
+const islogged =  false
+const outtemp = null
+let userEmail;
+const id = Symbol('123')
+const idd = Symbol('123')
+//console.log(id  === idd);
+const bigNumber = 97845619845623198456219845624n
+ 
+
+
+//non primitive or refrence datatype
+// return type of non  primitive types is object
+       // array
+        const heros = ['shaktiman', 'asfg']
+       // objects
+            let myobj={
+                name:'atul',
+                age:21
+            }
+       // functions
+           const myfun = function(){
+                console.log("hellli")
+           }
+         
+
+console.log(typeof score)
+console.log(typeof svalue)
+console.log(typeof islogged)
+console.log(typeof outtemp)
+console.log(typeof userEmail)
+console.log(typeof id)
+console.log(typeof idd)
+console.log(typeof bigNumber)
+console.log(typeof heros)
+console.log(typeof myobj)
+console.log(typeof myfun)
