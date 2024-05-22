36e124a61aae2caa8f4fd86c573da4ec3ec47faa
SHASHWAT SHUKLA <105039107+Shashwatshukl2040@users.noreply.github.com>
2024-05-22 10:35:57 +0000

    string start

---
+++ jscode/string.js
@@ -0,0 +1,11 @@
+// string 
+const name = 'atul'
+const repocnt = 50
+// console.log(name+repocnt)
+console.log(`Hello my name is ${name} and repocount is ${repocnt} `)
+
+
+const gamenw = new String ('Atul')
+ 
+console.log(gamenw[0])
+console.log(gamenw.__proto__)
