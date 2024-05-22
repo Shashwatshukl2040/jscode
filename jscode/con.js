cece045febc72041da986dbcc42249b486486c51
SHASHWAT SHUKLA <105039107+Shashwatshukl2040@users.noreply.github.com>
2024-05-22 05:34:33 +0000

    conversion of data types

--- jscode/con.js
+++ jscode/con.js
@@ -7,3 +7,20 @@ let valueInNumber= Number(score);
 
 console.log(typeof valueInNumber);
 console.log(valueInNumber); 
+
+// 33=> 33
+// 33abc=> NaN (Not a Number )
+// true => 1 ; false => 0;
+
+let isLoggedIn= ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLoggedI= "1"
+let booleanIsLoggedI = Boolean(isLoggedI)
+console.log(booleanIsLoggedI);
+// boolean 1 => true & 0 for false
+
+let someni=33
+let stringNm=String(someni)
+console.log(typeof stringNm);
